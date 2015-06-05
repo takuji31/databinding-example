2a2d3e2ad070532891c32af0572c42241099c8da
Takuji Nishibayashi <takuji31@gmail.com>
2015-06-05 11:45:51 +0900

    Initial commit

---
+++ app/src/main/java/jp/takuji31/databindingexample/ViewModel.java
@@ -0,0 +1,11 @@
+package jp.takuji31.databindingexample;
+
+import android.databinding.BaseObservable;
+
+/**
+ * @author Takuji Nishibayashi
+ */
+public class ViewModel extends BaseObservable {
+    //TODO first/last/full name
+    //TODO birthday
+}
